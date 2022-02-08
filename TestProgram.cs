ff3f444134a9f28b8dd8a30017c8fd7c15b8e91f
Avaz <hcoder@gmail.com>
2022-02-08 18:24:55 +0500

    Add project files.

---
+++ TestProgram.cs
@@ -0,0 +1,14 @@
+﻿
+
+using CitybankATMSystem;
+
+class TestProgram
+    {
+        static void Main()
+        {
+            CitybankATM atm = new CitybankATM();
+            atm.Initialization();
+            atm.Execute();
+        }
+    }
+
